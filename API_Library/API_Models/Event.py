249b8116f8bfcf90283bf3941c6c38a8a667a233
Michael Gross <mgstinger521@gmail.com>
2024-12-25 20:40:24 -0700

    Remove Extra API Lib folder

--- API_Library/API_Models/Event.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass,field
-from typing import Dict
-from Team import Stats as ts
-
-@dataclass
-class Event:
-    '''Stats for Tournament.'''
-    teams: Dict[int, ts] = field(default_factory=dict)
-    eventCode: str = ""
--- API_Library/RobotMath/TeamMatrixBuilder.py
+++
@@ -1,7 +0,0 @@
-from MatrixMath import LSE
-
-class MatrixBuilder(LSE):
-    def __init__(self, teams):
-        self.teams = teams
-        
-        super.__init__()
